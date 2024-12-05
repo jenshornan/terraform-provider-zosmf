2692eeb25861c0e5009a4c17e7116726877bed3f
Jens Hornstrom <jens.hornstrom@seb.se>
2024-12-05 09:44:04 +0100

    Add Doc

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 		// TODO: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
-		Address: "seb.se/provider/zosmf",
+		Address: "registry.terraform.io/jenshornan/zosmf",
 		Debug:   debug,
 	}
 
